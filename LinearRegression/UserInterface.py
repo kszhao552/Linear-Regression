89d8cfa2771b990b71d62c83a237a64f26657c72
Kradon <kradonzhao@gmail.com>
2020-10-28 22:37:06 -0400

    Update the functionality of the buttons in the initial window.
    Now if you press "Next" without an input, it will open a popup that will inform the user to select a file.
    If you press "Next" with a file selected, it will close the window.
    If you hit the manual input button, it will now close the window.
    Closing the window will still act the same way.

--- LinearRegression/UserInterface.py
+++ LinearRegression/UserInterface.py
@@ -10,7 +10,20 @@ window = sg.Window("Linear Regression", layout)
 
 while True:
     event, values = window.read()
-    if event == "Next" or event == sg.WIN_CLOSED:
+
+    #Want to create a new window, if event is next.
+    #But if the input file is empty, then we want to not continue on
+    if event == "Next":
+       if (values[0] == ""):
+           sg.popup("Please select a file.")
+       else:
+            break
+
+    #If the user wants to input values individually, then we must prompt the user to do so.
+    if event == "Input Values Manually":
+        break
+
+    if event == sg.WIN_CLOSED:
         break
 
 window.close()
