84c77d99cd17987ad77be650dc9b3360a40fe441
Kradon <kradonzhao@gmail.com>
2020-10-31 22:11:17 -0400

    feat: Implement basic functionality of csv analysis.
    The user interface will now analysis the data in the file.
    It will write to output and tell the user before closing.
    Removed manual input option for now.
    TODO: select which column you wish to use for analysis.
    Display the information in a new window.
    Add manual input functionality (?)

--- LinearRegression/UserInterface.py
+++ LinearRegression/UserInterface.py
@@ -1,12 +1,20 @@
-from LinearRegression import LinearRegression
-
+import LinearRegression
+from regression import Regression
+from vector import Vector
 import PySimpleGUI as sg
+import sys
 
 layout = [[sg.Text('Select a file:')], 
          [sg.FileBrowse(file_types=(("CSV files", "*.csv"),)), sg.Input()],
-         [sg.Button("Next"), sg.Button("Input Values Manually")]]
+         [sg.Button("Next"), sg.Button("Exit")]]
 
 window = sg.Window("Linear Regression", layout)
+
+line = Regression(0)
+x = Vector(0)
+y = Vector(0)
+errors = Vector(0)
+predicted = Vector(0)
 
 while True:
     event, values = window.read()
@@ -15,15 +23,30 @@ while True:
     #But if the input file is empty, then we want to not continue on
     if event == "Next":
        if (values[0] == ""):
-           sg.popup("Please select a file.")
+            sg.popup("Please select a file.")
        else:
-            break
+           #Attempt to create the linear regression data from the input file.
+           filePath = values[0]
+           try:
+                LinearRegression.initializeVectors(x, y, filePath)
+                LinearRegression.calculateLine(x, y, line)
+                predicted = LinearRegression.calculatePredicted(x, line)
+                errors = LinearRegression.calculateErrors(x, y, line, predicted, errors)
+                LinearRegression.writeToFile(x, y, line, predicted, errors)
+                sg.popup("Output has been created")
+                break
+           except ValueError:
+                sg.popup("Non-numerical value in data")
+           except ZeroDivisionError:
+               sg.popup("Values all on same x value or y value.")
 
     #If the user wants to input values individually, then we must prompt the user to do so.
-    if event == "Input Values Manually":
+    if event == "Exit":
         break
 
     if event == sg.WIN_CLOSED:
-        break
+        window.close()
+        sys.exit(0)
+
 
 window.close()
